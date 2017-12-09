19c08b3740d704b403e3067736d8674de7de9cb3
Aleksandr Shoronov <alexshoronov@gmail.com>
2017-12-09 14:11:22 +0300

    Added API for Trello

--- config/default.js
+++ config/default.js
@@ -1,3 +1,11 @@
 module.exports = {
-	token: ''
-}
+	telegram: {
+		token: process.env.TELEGRAM_API_KEY,
+		username: process.env.TELEGRAM_API_USERNAME
+	},
+	trello: {
+		key: process.env.TRELLO_KEY,
+		token: process.env.TRELLO_TOKEN,
+		list: process.env.TRELLO_LIST
+	}
+};
--- src/index.js
+++ src/index.js
@@ -1,24 +1,44 @@
 const config = require('config');
-const Telegraf = require('telegraf')
+const Telegraf = require('telegraf');
 
-const adjectives = require('./adjectives')
+const adjectives = require('./adjectives');
+const trello = require('./trello');
 
 const {
+	add,
 	greeting
 } = require('./templates');
 
-const bot = new Telegraf(config.token, {
-	username: 'LeninMolodoyBot'
+const bot = new Telegraf(config.telegram.token, {
+	username: config.telegram.username
 });
 
+const getCardContent = ({message: {text}}) => {
+	const [greeting, ...parts] = text.split(' ');
+	const [name, desc] = parts.join('').split(/\n\n/);
+
+	return {
+		name,
+		desc
+	};
+}
+
 bot.start((ctx) => {
-	return ctx.reply('Йо, Йо, Йо!')
+	return ctx.reply('Йо, Йо, Йо!');
 });
 
 bot.command('greating', (ctx) => {
 	ctx.reply(greeting(adjectives.getAdjective()), {
 		parse_mode: 'Markdown'
 	})
+});
+
+bot.command('add', (ctx) => {
+	trello
+		.addCard(getCardContent(ctx))
+		.then(({body: {url}}) => {
+			ctx.reply(add(url));
+		});
 });
 
 bot.startPolling()
