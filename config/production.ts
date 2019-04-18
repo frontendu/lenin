9879d7dab8c235d4c5612920c9631ad5314e7c9c
Aleksandr Shoronov <alexshoronov@gmail.com>
2019-04-18 15:41:22 +0300

    Added host and port

--- config/production.ts
+++ config/production.ts
@@ -5,6 +5,8 @@ module.exports = {
   telegram: {
     launch: {
       webhook: {
+        port: process.env.PORT,
+        host: process.env.HOST,
         domain: process.env.TG_API_DOMAIN
       }
     }
