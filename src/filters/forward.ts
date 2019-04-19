9442a64b35f6bbeb45e997c89092d95fb2ec9320
Aleksandr Shoronov <alexshoronov@gmail.com>
2019-04-19 11:24:35 +0300

    Fixed condition

--- src/filters/forward.ts
+++ src/filters/forward.ts
@@ -1,7 +1,7 @@
 import { ContextMessageUpdate } from 'telegraf';
 
 export const isForward = (ctx: ContextMessageUpdate) => {
-  const text = ctx.message.text || ctx.message.caption;
+  const text = ctx.message && (ctx.message.text || ctx.message.caption);
   const includeLinks = text.includes('http://') || text.includes('https://');
   const forwardFromChat =
     ctx.message && typeof ctx.message.forward_from_chat === 'object';
