0e54adb4eb12e17f731ab782bad2a69793dbad1c
Aleksandr Shoronov <alexshoronov@gmail.com>
2020-06-20 20:07:51 +0300

    Fixed fetch title for twitter

--- src/services/site.service.ts
+++ src/services/site.service.ts
@@ -2,8 +2,10 @@ import got from 'got';
 
 import { ISiteService } from 'interfaces/site.interface';
 
+const CRAWLER_HEADER = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+
 export class SiteService implements ISiteService {
   public async getContent(url: string) {
-    return (await got(url)).body;
+    return (await got(url, { headers: { 'User-Agent': CRAWLER_HEADER } })).body;
   }
 }
